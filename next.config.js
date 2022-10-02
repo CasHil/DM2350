8e0393152c220e94f87114833cf610b89ca0f3e7
Casper Hildebrand <casperhildebrand@gmail.com>
2022-10-03 01:07:03 +0200

    Add basepath config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  basePath: isProd ? "/DM2350" : "",
   assetPrefix: isProd ? "/DM2350/" : "/",
   images: {
     unoptimized: true,
