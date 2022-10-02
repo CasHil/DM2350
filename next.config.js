490ca67cc33ae01336ab07fffcfbb26cd94462fc
Casper Hildebrand <casperhildebrand@gmail.com>
2022-10-02 22:50:03 +0200

    Setup Github Actions for deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
-}
+  assetPrefix: isProd ? "/dm2350/" : "",
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
