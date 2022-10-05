23646a349d201264953f7204a36d74af51158bd7
Casper Hildebrand <casperhildebrand@gmail.com>
2022-10-05 16:57:44 +0200

    Update experiment description

--- pages/index.js
+++ pages/index.js
@@ -32,13 +32,17 @@ export default function Home() {
           <h1>Perceptual preferences for groove patterns in jazz</h1>
           <p>
             Thank you for participating in our study about perceptual
-            preferences in jazz. Our goal in this research is to find out the
-            preferences of listeners, varying specific aspects of the song that
-            you will be listening to. First you will be asked for some
-            statistics about yourself. After that, you will listen to 25
-            samples, which you will have to rate on a scale from 1 to 5. All
-            your data is completely anonymous and will be stored only for the
-            duration of the course, after which it will be discarded.
+            preferences in jazz! Our goal in this research is to find out the
+            preferences of listeners, varying specific aspects (tempo, swing
+            ratio and delays of the soloist) of the song that you will be
+            listening to. It will take you about <b>10 minutes</b>. First you
+            will be asked for some statistics about yourself. After that, you
+            will listen to 25 samples, which you will have to rate on a scale
+            from 1 to 5, where 1 means liking it the least and 5 means liking it
+            the most. Similarly, you will be asked how groovy you find the
+            sample. All of the samples are adapted from one piece of jazz. Note:
+            All your data is completely anonymous and will be stored only for
+            the duration of the course, after which it will be discarded.
           </p>
           <Divider />
           <Auth />
