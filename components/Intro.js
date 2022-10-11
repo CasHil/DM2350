d80a4eca5aa5f3130c5b065b6d0004ce846873a4
Casper Hildebrand <casperhildebrand@gmail.com>
2022-10-12 00:39:51 +0200

    Update description

--- components/Intro.js
+++ components/Intro.js
@@ -11,9 +11,11 @@ export default function Intro() {
       yourself. After that, you will listen to 25 samples, which you will have
       to rate how much you like or dislike them and how groovy you think they
       are. All of the samples are adapted from one piece of jazz. Make sure that
-      you listen to the samples with headphones in and in a quiet environment.{" "}
-      <br /> Note: All your data is completely anonymous and will be stored only
-      for the duration of the course, after which it will be discarded.
+      you listen to the samples with headphones in and in a quiet environment.
+      Please use either <b>Chrome</b> or <b>Safari</b> to ensure that the
+      website works correctly. <br /> All your data is completely anonymous and
+      will be stored only for the duration of the course, after which it will be
+      discarded.
     </p>
   );
 }
